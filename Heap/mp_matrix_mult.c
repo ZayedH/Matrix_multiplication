ecd0d5bfc5d46dba0f7410f3ae33ea4cfb224d19
ZayedH <zayed@cpp>
2023-10-08 21:08:37 +0200

    openMP

--- Heap/mp_matrix_mult.c
+++ Heap/mp_matrix_mult.c
@@ -15,6 +15,7 @@ int main(int argc, char **argv)
     int* A;
     int* B;
     int* C;
+    int i, j, k;
     int n_rows_cols = 1458;
     A = malloc(sizeof(int) * n_rows_cols*n_rows_cols);
     B = malloc(sizeof(int) * n_rows_cols*n_rows_cols);
@@ -25,38 +26,45 @@ int main(int argc, char **argv)
                 *(B+i*n_rows_cols + j) = rand()% 5;
       }
    }
-    #pragma omp parallel 
-    {
-        int number_of_threads = omp_get_num_threads();
-        int rows = n_rows_cols/number_of_threads;
-        int rank_of_thread = omp_get_thread_num();
-        if ( rank_of_thread != number_of_threads-1){
+    #pragma omp parallel for private(i,j,k)
+    
+        // int number_of_threads = omp_get_num_threads();
+        // int rows = n_rows_cols/number_of_threads;
+        // int rank_of_thread = omp_get_thread_num();
+        // if ( rank_of_thread != number_of_threads-1){
 
-        for (int i = rows*rank_of_thread; i < n_rows_cols ; i++)
-         {
-         for (int j = 0; j < n_rows_cols; j++)
-             {
-                *(C+i*n_rows_cols+j) = 0;
-             for (int k = 0; k < n_rows_cols; k++)
-              {
-                  *(C+i*n_rows_cols+j) +=  *(A+i*n_rows_cols+k) * *(B+k*n_rows_cols+j);
-                }
-            }
-         }
+        // for (int i = rows*rank_of_thread; i < n_rows_cols ; i++)
+        //  {
+        //  for (int j = 0; j < n_rows_cols; j++)
+        //      {
+        //         *(C+i*n_rows_cols+j) = 0;
+        //      for (int k = 0; k < n_rows_cols; k++)
+        //       {
+        //           *(C+i*n_rows_cols+j) +=  *(A+i*n_rows_cols+k) * *(B+k*n_rows_cols+j);
+        //         }
+        //     }
+        //  }
 
-        }else{
-        for (int i = rows*rank_of_thread; i < (rank_of_thread+1)*rows ; i++)
-         {
-         for (int j = 0; j < n_rows_cols; j++)
-             {
-                *(C+i*n_rows_cols+j) = 0;
-             for (int k = 0; k < n_rows_cols; k++)
-              {
-                  *(C+i*n_rows_cols+j) +=  *(A+i*n_rows_cols+k) * *(B+k*n_rows_cols+j);
-                }
-            }
+        // }else{
+        // for (int i = rows*rank_of_thread; i < (rank_of_thread+1)*rows ; i++)
+        //  {
+        //  for (int j = 0; j < n_rows_cols; j++)
+        //      {
+        //         *(C+i*n_rows_cols+j) = 0;
+        //      for (int k = 0; k < n_rows_cols; k++)
+        //       {
+        //           *(C+i*n_rows_cols+j) +=  *(A+i*n_rows_cols+k) * *(B+k*n_rows_cols+j);
+        //         }
+        //     }
+        //  }
+        // }
+        for(i = 0; i <  n_rows_cols; i++) {
+            for(j = 0; j <  n_rows_cols; j++) {
+         *(C+i*n_rows_cols + j)= 0;
+         for(k = 0; k < n_rows_cols; k++) {
+             *(C+i*n_rows_cols + j)+= *(A+i*n_rows_cols+k) * *(B+k*n_rows_cols+j);
          }
-        }
+      } 
         
     }
     clock_t end = clock();
